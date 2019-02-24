4fe0986430f9a5c9d88c646ec7d3858ef50bfdc9
Marc Miron <m_miron@yahoo.com>
2019-02-24 03:15:19 +0000

    added es6 code

--- ts-test/src/index.js
+++ ts-test/src/index.js
@@ -1,7 +1,8 @@
 // import '@babel/polyfill';
 const greetings = (name) => {
     return `hello ${name}`;
-}
+};
+
 console.log(greetings('Steve'));
 
 console.log('Hello world');
@@ -11,3 +12,192 @@ var array = [1, 2, 3];
 Array.from(array).forEach(($item) => {
     console.log($item);
 });
+
+var str = "Hello world, welcome to the universe.";
+var n = str.includes("world");
+console.log("n", n);
+
+var promise1 = new Promise(function(resolve, reject) {
+    setTimeout(function() {
+        resolve('foo');
+    }, 300);
+});
+
+promise1.then(function(value) {
+    console.log(value);
+    // expected output: "foo"
+});
+
+console.log(promise1);
+// expected output: [object Promise]
+
+
+var materials = [
+    'Hydrogen',
+    'Helium',
+    'Lithium',
+    'Beryllium'
+];
+
+console.log(materials.map(material => material.length));
+// expected output: Array [8, 6, 7, 9]
+
+
+function sum(...theArgs) {
+    return theArgs.reduce((previous, current) => {
+        return previous + current;
+    });
+}
+
+console.log(sum(1, 2, 3));
+// expected output: 6
+
+console.log(sum(1, 2, 3, 4));
+// expected output: 10
+
+const numbers = [1, 2, 3];
+
+console.log(sum(...numbers));
+// expected output: 6
+
+console.log(sum.apply(null, numbers));
+// expected output: 6
+
+
+let list = [8, 3, 11, 9, 6],
+    length = list.length,
+    i;
+
+for (i = 0; i < length; i++) {
+    console.log(list[i]);
+}
+
+list.forEach(function(value, i) {
+    console.log(value);
+});
+
+
+for (let value of list) {
+    console.log(value);
+}
+
+var a, b, rest;
+[a, b] = [10, 20];
+
+console.log(a);
+// expected output: 10
+
+console.log(b);
+// expected output: 20
+
+[a, b, ...rest] = [10, 20, 30, 40, 50];
+
+console.log(rest);
+// expected output: [30,40,50]
+
+export default async function foo() {
+    var s = await bar();
+    console.log(s);
+}
+
+function bar() {
+    return "bar";
+}
+
+new foo();
+
+function* generator(i) {
+    yield i;
+    yield i + 10;
+}
+
+var gen = generator(10);
+
+console.log(gen.next().value);
+// expected output: 10
+
+console.log(gen.next().value);
+// expected output: 20
+
+// create a TypedArray with a size in bytes
+const typedArray1 = new Int8Array(8);
+typedArray1[0] = 32;
+
+const typedArray2 = new Int8Array(typedArray1);
+typedArray2[1] = 42;
+
+console.log(typedArray1);
+// expected output: Int8Array [32, 0, 0, 0, 0, 0, 0, 0]
+
+console.log(typedArray2);
+// expected output: Int8Array [32, 42, 0, 0, 0, 0, 0, 0]
+
+
+var myMap = new Map();
+
+var keyString = 'a string',
+    keyObj = {},
+    keyFunc = function() {};
+
+// setting the values
+myMap.set(keyString, "value associated with 'a string'");
+myMap.set(keyObj, 'value associated with keyObj');
+myMap.set(keyFunc, 'value associated with keyFunc');
+
+myMap.size; // 3
+
+// getting the values
+myMap.get(keyString); // "value associated with 'a string'"
+myMap.get(keyObj); // "value associated with keyObj"
+myMap.get(keyFunc); // "value associated with keyFunc"
+
+myMap.get('a string'); // "value associated with 'a string'"
+// because keyString === 'a string'
+myMap.get({}); // undefined, because keyObj !== {}
+myMap.get(function() {}); // undefined, because keyFunc !== function () {}
+
+var wm1 = new WeakMap(),
+    wm2 = new WeakMap(),
+    wm3 = new WeakMap();
+var o1 = {},
+    o2 = function() {},
+    o3 = window;
+
+wm1.set(o1, 37);
+wm1.set(o2, 'azerty');
+wm2.set(o1, o2); // a value can be anything, including an object or a function
+wm2.set(o3, undefined);
+wm2.set(wm1, wm2); // keys and values can be any objects. Even WeakMaps!
+
+wm1.get(o2); // "azerty"
+wm2.get(o2); // undefined, because there is no key for o2 on wm2
+wm2.get(o3); // undefined, because that is the set value
+
+wm1.has(o2); // true
+wm2.has(o2); // false
+wm2.has(o3); // true (even if the value itself is 'undefined')
+
+wm3.set(o1, 37);
+wm3.get(o1); // 37
+
+wm1.has(o1); // true
+wm1.delete(o1);
+wm1.has(o1); // false
+
+
+var target = { foo: 'bar', baz: 'wat' };
+
+Reflect.deleteProperty(target, 'foo');
+console.log(target);
+
+const symbol1 = Symbol();
+const symbol3 = Symbol('foo');
+
+console.log(typeof symbol1);
+// expected output: "symbol"
+
+console.log(symbol3.toString());
+// expected output: "Symbol(foo)"
+
+console.log(Symbol('foo') === Symbol('foo'));
+// expected output: false
